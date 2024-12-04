ea543bbf5dfe8ca0c682d74b017d35c4c37b41c2
sidney-prytherch <sidneyprytherch@gmail.com>
2024-12-04 16:33:09 -0500

    Update routes

--- src/routes/oldpage.svelte
+++ src/routes/oldpage.svelte
@@ -1,14 +1,3 @@
-<script lang="ts">
-	import { enhance } from '$app/forms';
-	import type { PageServerData } from './$types';
-
-	let { data }: { data: PageServerData } = $props();
-</script>
-
-<h1>Hi, {data.user.username}!</h1>
-<p>Your user ID is {data.user.id}.</p>
-
-
 <!-- <script lang="ts">
     import {TENSES} from './VerbForm';
 </script> -->
@@ -21,8 +10,3 @@
     type="text"
 
 />
-
-
-<form method="post" action="?/logout" use:enhance>
-	<button>Sign out</button>
-</form>
--- src/routes/user/presets/+page.svelte
+++ src/routes/user/presets/+page.svelte
@@ -6,7 +6,3 @@
 </script>
 
 <h1>Hi, {data.user.username}!</h1>
-<p>Your user ID is {data.user.id}.</p>
-<form method="post" action="?/logout" use:enhance>
-	<button>Sign out</button>
-</form>
